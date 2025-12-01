4028c1e0c358c8fc9f74c0cf0e7de05382fb25e1
rafa4321 <108878060+rafa4321@users.noreply.github.com>
2025-12-01 12:22:35 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Asegúrate de que este archivo App.js esté en src/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
