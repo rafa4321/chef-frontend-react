89af1a3f44de6b903a1aa8b05382798a4e4990a9
rafa4321 <108878060+rafa4321@users.noreply.github.com>
2025-12-02 08:38:58 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>¡El Despliegue Finalmente Funcionará!</h1>
+      {/* Tu código de aplicación va aquí */}
+    </div>
+  );
+}
+export default App;
